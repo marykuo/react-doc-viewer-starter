dc8d948f5166f618fc2a7c72f08152d19cb1e394
marykuo <meiyu19980928@gmail.com>
2026-02-13 19:58:09 +0800

    feat: replace remark-remove-comments with rehypeRaw for rendering raw HTML

--- src/components/MarkdownView.jsx
+++ src/components/MarkdownView.jsx
@@ -1,12 +1,11 @@
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
-import remarkRemoveComments from "remark-remove-comments";
+import rehypeRaw from "rehype-raw";
 
 export default function MarkdownView({ content }) {
   return (
     <article>
-      {/* remarkGfm 讓 Markdown 支援表格、任務列表等功能 */}
-      <ReactMarkdown remarkPlugins={[remarkGfm, remarkRemoveComments]}>
+      <ReactMarkdown remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeRaw]}>
         {content}
       </ReactMarkdown>
     </article>
