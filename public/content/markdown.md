4392f0770c93349ab2c0b1b8efd6d3d98912e7b3
marykuo <meiyu19980928@gmail.com>
2026-02-15 18:00:22 +0800

    style: add remark-github-alerts for rendering github alert

--- public/content/markdown.md
+++ public/content/markdown.md
@@ -121,6 +121,23 @@ _斜體_
 <font color="#fd4340"> 紅色 </font>
 <font color="#F7A004"> 橘色 </font>
 
+### GitHub Alert
+
+> [!NOTE]
+> Highlights information that users should take into account, even when skimming.
+
+> [!TIP]
+> Optional information to help a user be more successful.
+
+> [!IMPORTANT]
+> Crucial information necessary for users to succeed.
+
+> [!WARNING]
+> Critical content demanding immediate user attention due to potential risks.
+
+> [!CAUTION]
+> Negative potential consequences of an action.
+
 ## 圖片
 
 ```markdown
--- src/components/MarkdownView.jsx
+++ src/components/MarkdownView.jsx
@@ -1,13 +1,14 @@
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import rehypeRaw from "rehype-raw";
+import remarkGithubAlerts from "remark-github-alerts";
 import remarkFlexibleMarkers from "remark-flexible-markers";
 
 export default function MarkdownView({ content }) {
   return (
     <article>
       <ReactMarkdown
-        remarkPlugins={[remarkGfm, remarkFlexibleMarkers]}
+        remarkPlugins={[remarkGfm, remarkFlexibleMarkers, remarkGithubAlerts]}
         rehypePlugins={[rehypeRaw]}
       >
         {content}
